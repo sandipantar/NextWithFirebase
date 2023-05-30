746982d21a5aeda1212d82be1501dc6ffc148cd8
sandipantar <sandipantar@gmail.com>
2023-05-30 12:19:29 +0530

    Delete src directory

--- src/pages/components/catagory/CatagoryContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const CatagoryContext = createContext(null);
--- src/pages/components/product/proEdit.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const proEdit = () => {
-  return (
-    <div>proEdit</div>
-  )
-}
-
-export default proEdit
